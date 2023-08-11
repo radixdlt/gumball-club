b3983acd85eb9156669bc4670583c9a985a78abd
jake-rdxworks <jakephucmai@gmail.com>
2023-08-11 13:00:14 -0400

    Documentation update

--- gumball_club/src/candy_machine.rs
+++ gumball_club/src/candy_machine.rs
@@ -17,6 +17,15 @@ mod candy_machine {
         }
     );
 
+    // Defines component method authority
+    
+    // --Roles--
+    // * `OWNER` -  The owner of the component which has authority to change resource and component 
+    //    metadata. 
+    // * `SELF` - The component itself which is designated authority to mint candies.
+    // * `member` - The member which is specified by those who hold the badge resource determined by 
+    //    the `member_card_address`.
+    // * `PUBLIC` - The role that defines everybody else.
     enable_method_auth! {
         roles {
             member => updatable_by: [SELF,OWNER];
@@ -32,6 +41,17 @@ mod candy_machine {
         }
     }
 
+    /// The `CandyMachine` holds these state in its component:
+
+    /// * `candy_token_manager` | `ResourceManager` - Holds the `ResourceManager` of the 
+    ///     candy tokens to reference and mint from.
+    
+    /// * `collected_tokens` | `Vault` - A `Vault` that holds and collects all the payment 
+    ///     resource determined by the `payment_token_address` it receives from the candies 
+    ///     it sells.
+    
+    /// * `discount_amount` | `Decimal` - Holds the discount amount that `member` role can 
+    ///     qualify for holding the specified `member_card_address`. 
     struct CandyMachine {
         candy_token_manager: ResourceManager,
         collected_tokens: Vault,
@@ -40,15 +60,55 @@ mod candy_machine {
 
     impl CandyMachine {
 
+    /// The `CandyMachine` uses the `instantiate_candy_machine` function to instantiate its 
+    /// component and it takes in 3 argument inputs:
+
+    /// 1. `owner_role` | `OwnerRole` - This is to define the "owner" of the components and 
+    ///     resource as well as map the `AccessRule` of the owner to a badge address.
+    ///     - The `OwnerRole` can be specified to be `Fixed` meaning that the badge address 
+    ///     that the `AccessRule` is mapped to will forever remain immutable.
+    ///     - The `OwnerRole` can be specified to be `Updatable` meaning the badge address 
+    ///     that the `AccessRule` is mapped to can be updated by the owner itself.
+    ///     - The `OwnerRole` can be specified to `None` meaning that the component and its 
+    ///     resources will not have an owner and will not be able to have access to privileged 
+    ///     methods nor update the entities metadata.
+    ///     - It is important to specify an `OwnerRole` and configure it to a badge address to 
+    ///     enable [metadata verification](https://docs-babylon.radixdlt.com/main/standards/metadata-for-verification.html).
+    /// 2. `payment_token_address` | `ResourceAddress` - This is the `ResourceAddress` of the 
+    ///     resource used for payment to purchase candy from this component.
+    /// 3. `member_card_address` | `ResourceAddress` - This is the `ResourceAddress` of a member 
+    ///     card badge resource used to provide special discounts to candies sold and minted by this component. 
+
+    /// You may use the `GumballClub` blueprint to automatically instantiate the `CandyMachine` 
+    /// with pre-configured `payment_token_address` and `member_card_address` provided by the 
+    /// `GumballClub`. If you wish to instantiate the `CandyMachine` on its own, then you must 
+    /// specify its own `payment_token_address` and `member_card_address` for your own purpose.
         pub fn instantiate_candy_machine(
             owner_role: OwnerRole,
             payment_token_address: ResourceAddress, 
             member_card_address: ResourceAddress,
         ) -> Global<CandyMachine> {
 
+            // Create a `GlobalAddressReservation` and `ComponentAddress` to use as the component's 
+            // "virtual actor badge".
             let (address_reservation, component_address) = 
                 Runtime::allocate_component_address(Runtime::blueprint_id());
             
+            // The resource definition for the candy token. This resource has divisibility
+            // set to 0 to ensure candies sold are whole candies and cannot be fractionalized.
+
+            // *Resource Behaviors*
+            // * Mintable - This resource can be mintable by the minter role.
+
+            // --Roles--
+            // * `minter` - The role that has authority to mint this resource. This role's 
+            // `AccessRule` is mapped to the component. 
+            // * `minter_updater` - The role that has authority to mutate the `AccessRule`
+            // of the `minter` role. This role's `AccessRule` is mapped to `DenyAll`, meaning
+            // the `minter` will always be the component.
+
+            // --Supply--
+            // This resource has no finite supply.
             let candy_token_manager = 
                 ResourceBuilder::new_fungible(owner_role.clone())
                 .divisibility(DIVISIBILITY_NONE)
@@ -91,6 +151,14 @@ mod candy_machine {
             .globalize()
         }
 
+        /// * `get_price` - Retrieves the current price of candy saved in component state. It uses an external 
+        ///     blueprint package cross-component call to the `SugarPriceOracle` to retrieve the price per candy.
+        /// 
+        /// --INPUTS--
+        /// * None
+        /// 
+        /// --RETURNS--
+        /// * `Decimal` - The price of candies in `Decimal` type.
         pub fn get_price(&mut self) -> Decimal {
             let mut sugar_price_oracle: Global<SugarPriceOracle> = global_component!(
                 SugarPriceOracle,
@@ -101,16 +169,43 @@ mod candy_machine {
             sugar_price_oracle.get_price()
         }
 
-        pub fn buy_candy(&mut self, mut payment: Bucket) -> (Bucket, Bucket) {
+        /// * `buy_candy` - Takes in a `Bucket` of resource specified by `payment_token_address` and mints candy 
+        ///     based on the cross-component call to `SugarPriceOracle` to determine the price per candy.
+        /// --INPUTS--
+        /// * `payment`: `Bucket` - Takes in a `Bucket` of payment specified by
+        /// `payment_token_address`.
+        
+        /// --RETURNS--
+        /// * `(Bucket, Bucket)` - Returns a tuple of `Bucket`:
+        /// 1. Any refunded/leftover payments to the user.
+        /// 2. The `Bucket` of candy.
+        pub fn buy_candy(&mut self, mut payment: Bucket) -> (Bucket, Bucket) {    
 
+            let resource_name_metadata: GlobalAddress =  
+                self.collected_tokens
+                    .resource_manager()
+                    .get_metadata("name")
+                    .unwrap_or(
+                        self.collected_tokens.resource_address().into()
+                    );            
+
+            // Asserts that the payment type is of the accepted payment resource.
+            // This assert is not needed but can be used for friendly error messages.
             assert_eq!(
                 payment.resource_address(), self.collected_tokens.resource_address(),
                 "[CandyMachine]: Only {:?} tokens are accepted as payments!", 
-                self.collected_tokens.resource_address()
+                resource_name_metadata
             );
+            
             
             let price_per_candy = self.get_price();
 
+            // Calculate the total amount of candy based on the amount of payment sent.
+            // The conditional statement is used whereby if the payment is less than
+            // the unit price of the candy, then the full amount of payment will be
+            // refunded and no candys will be returned. If the payment is sufficient,
+            // then the total candy amount is calculated and rounded down to ensure
+            // only whole candys can be minted.
             let total_candy_amount = if payment.amount() < price_per_candy {
                 dec!(0)
             } else {
@@ -126,12 +221,33 @@ mod candy_machine {
             return (self.candy_token_manager.mint(total_candy_amount), payment)
         }
 
+        /// * `buy_candy_with_member_card` - Authorized method for `member` role which requires a `Proof` of 
+        ///     the resource specified by `member_card_address`. If `Proof` is provided, then candies are 50% off.
+        /// 
+        /// --INPUTS--
+        /// * `payment`: `Bucket` - Takes in a `Bucket` of payment specified by
+        /// `payment_token_address`.
+        
+        /// --RETURNS--
+        /// * `(Bucket, Bucket)` - Returns a tuple of `Bucket`:
+        /// 1. Any refunded/leftover payments to the user.
+        /// 2. The `Bucket` of candy.
         pub fn buy_candy_with_member_card(&mut self, mut payment: Bucket) -> (Bucket, Bucket) {
 
+            let resource_name_metadata: GlobalAddress =  
+                self.collected_tokens
+                    .resource_manager()
+                    .get_metadata("name")
+                    .unwrap_or(
+                        self.collected_tokens.resource_address().into()
+                    );     
+
+            // Asserts that the payment type is of the accepted payment resource.
+            // This assert is not needed but can be used for friendly error messages.
             assert_eq!(
                 payment.resource_address(), self.collected_tokens.resource_address(),
                 "[CandyMachine]: Only {:?} tokens are accepted as payments!", 
-                self.collected_tokens.resource_address()
+                resource_name_metadata
             );
             
             let price_per_candy = self.get_price();
@@ -154,6 +270,15 @@ mod candy_machine {
 
         }
 
+        /// * `change_discount` - Authorized method for `OWNER` role which requires a `Proof` of the resource 
+        ///     specified by `owner_role`. If `Proof` is provided, then new discount amount may be inputted. 
+        
+        /// --INPUTS--
+        /// * `new_discount_amount` | `Decimal` - The new discount amount members can qualify
+        /// for candies purchased inputted as a `Decimal` type.
+        
+        /// --RETURNS--
+        /// * None
         pub fn change_discount(&mut self, new_discount_amount: Decimal) {
             assert!(
                 (new_discount_amount >= Decimal::zero()) & (new_discount_amount <= dec!("100")), 
@@ -163,14 +288,39 @@ mod candy_machine {
             self.discount_amount = new_discount_amount;
         }
 
+        /// * `change_member_card` - Authorized method for `OWNER` role which requires a `Proof` of the resource 
+        /// specified by `owner_role`. If `Proof` is provided, then new `member_card_address` may be inputted. 
+         
+        /// --INPUTS--
+        /// * `new_member_card` | `ResourceAddress` - The new member card that will be associated
+        /// with the `member` role to qualify for special discounts inputted as a `ResourceAddress`.
+        
+        /// --RETURNS--
+        /// * None
         pub fn change_member_card(&mut self, new_member_card: ResourceAddress) {
             Runtime::global_component().set_role("member", rule!(require(new_member_card)));
         }
 
+        /// * `get_discount` - Retrieves the current discount amount a member can qualify for the candies.
+         
+        /// --INPUTS--
+        /// * None
+        
+        /// --RETURNS--
+        /// * `Decimal` - Retrieves the discount amount that members can qualify for the 
+        /// purchase of candies returned as a `Decimal` type.
         pub fn get_discount(&self) -> Decimal {
             self.discount_amount
         }
 
+        /// * `get_member_role` - Retrieves the `AccessRule` of the `member` role.
+         
+        /// --INPUTS--
+        /// * None
+         
+        /// --RETURNS--
+        /// `AccessRule` - Returns the `member` role's `AccessRule` which maps to the member
+        /// card's `ResourceAddress.
         pub fn get_member_role(&self) -> AccessRule {
             Runtime::global_component().get_role("member").unwrap()
         }
--- gumball_club/src/gumball_club.rs
+++ gumball_club/src/gumball_club.rs
@@ -4,11 +4,23 @@ use crate::gumball_machine::gumball_machine::GumballMachineFunctions;
 use crate::candy_machine::candy_machine::CandyMachine;
 use crate::candy_machine::candy_machine::CandyMachineFunctions;
 
+/// The `NonFungibleData` structure of the member card NFT badge.
 #[derive(ScryptoSbor, NonFungibleData)]
 pub struct GumballClubMember {}
 
 #[blueprint]
 mod gumball_club {
+
+    /// The `GumballClub` holds these state in its component:
+
+    /// * `gumball_club_token_manager` | `ResourceManager` - Holds the `ResourceManager` of the Gumball Club 
+    ///    tokens to reference and mint from.
+    /// * `member_card_manager` | `ResourceManager` - Holds the `ResourceManager` of the member card NFT badge 
+    ///    to reference and mint from.
+    /// * `collected_gc_vault` | `Vault` - A `Vault` that holds and collects all the payment resource in Gumball 
+    ///    Club tokens it receives from the member cards it sells.
+    /// * `gumball_club_member_counter` | `u64` - Holds and tracks the member card NFT badges is minted and 
+    ///    determines its `NonFungibleLocalId`
     struct GumballClub {
         gumball_club_token_manager: ResourceManager,
         member_card_manager: ResourceManager,
@@ -18,6 +30,23 @@ mod gumball_club {
     }
 
     impl GumballClub {
+
+    /// The `GumballClub` instantiation function: `instantiate_gumball_club` takes in 3 argument inputs:
+
+    /// 1. `owner_role` | `OwnerRole` - This is to define the "owner" of the components and resource as 
+    ///     well as map the `AccessRule` of the owner to a badge address.
+    ///     - The `OwnerRole` can be specified to be `Fixed` meaning that the badge address that the `AccessRule`
+    ///     is mapped to will forever remain immutable.
+    ///     - The `OwnerRole` can be specified to be `Updatable` meaning the badge address that the `AccessRule` 
+    ///     is mapped to can be updated by the owner itself.
+    ///     - The `OwnerRole` can be specified to `None` meaning that the component and its resources will not 
+    ///     have an owner and will not be able to have access to privileged methods nor update the entities metadata.
+    ///     - It is important to specify an `OwnerRole` and configure it to a badge address to enable [
+    ///     metadata verification](https://docs-babylon.radixdlt.com/main/standards/metadata-for-verification.html).
+    /// 2. `price_per_card` | `Decimal` - This is to confgiure the price of the member card that will be minted and 
+    ///     sold by the `GumballClub` component.
+    /// 3. `price_per_gumball` | `Decimal` - This is the configure the price of the gumball resource minted and sold 
+    ///     by the `GumballMachine`.
         pub fn instantiate_gumball_club(
             owner_role: OwnerRole,
             price_per_card: Decimal,
@@ -28,9 +57,26 @@ mod gumball_club {
             Global<CandyMachine>
         ) {
 
+            // Create a `GlobalAddressReservation` and `ComponentAddress` to use as the component's 
+            // "virtual actor badge".
             let (address_reservation, component_address) =
                 Runtime::allocate_component_address(Runtime::blueprint_id());       
-            
+
+            // The resource definition for the candy token. This resource has divisibility
+            // set to maximum of 18 as the Gumball Club token is designed to be fractionalized.
+
+            // *Resource Behaviors*
+            // * Mintable - This resource can be mintable by the minter role.
+
+            // --Roles--
+            // * `minter` - The role that has authority to mint this resource. This role's 
+            // `AccessRule` is mapped to the component. 
+            // * `minter_updater` - The role that has authority to mutate the `AccessRule`
+            // of the `minter` role. This role's `AccessRule` is mapped to `DenyAll`, meaning
+            // the `minter` will always be the component.
+
+            // --Supply--
+            // This resource has no finite supply.
             let gumball_club_token_manager: ResourceManager = 
                 ResourceBuilder::new_fungible(owner_role.clone())
                 .divisibility(DIVISIBILITY_MAXIMUM)
@@ -46,6 +92,24 @@ mod gumball_club {
                 })
                 .create_with_no_initial_supply();
 
+            // The resource definition for the member card. The `NonFungibleLocalId`
+            // of this resource is set to `Integer` type.
+
+            // --NonFungibleData--
+            // * None
+
+            // *Resource Behaviors*
+            // * Mintable - This resource can be mintable by the minter role.
+
+            // --Roles--
+            // * `minter` - The role that has authority to mint this resource. This role's 
+            // `AccessRule` is mapped to the component. 
+            // * `minter_updater` - The role that has authority to mutate the `AccessRule`
+            // of the `minter` role. This role's `AccessRule` is mapped to `DenyAll`, meaning
+            // the `minter` will always be the component.
+
+            // --Supply--
+            // This resource has no finite supply.
             let member_card_manager: ResourceManager = 
                 ResourceBuilder::new_integer_non_fungible::<GumballClubMember>(owner_role.clone())
                 .metadata(metadata! {
@@ -104,14 +168,30 @@ mod gumball_club {
 
         }
 
+        /// * `dispense_gc_tokens` - This method, when called, mints the caller with 20 Gumball Club tokens. 
+        ///    A user may continuously call this method to mint more Gumball Club tokens, 20 at a time.
         
+        /// --INPUTS--
+        /// * None
+        /// --RETURNS--
+        /// * `Bucket` - Returns a `Bucket` of Gumball Club token at hardcoded amount of 20.
         pub fn dispense_gc_tokens(&mut self) -> Bucket {
-            // Currently mint amount hard coded to 20
+            // Currently mint amount hard coded to 100 for testing purposes.
             let gumball_club_tokens = self.gumball_club_token_manager.mint(dec!(100));
 
             return gumball_club_tokens
         }
 
+        /// * `buy_member_card` - This takes in a payment `Bucket` of Gumball Club tokens. For 5 Gumball Club tokens, 
+        ///    the caller will receive 1 Member Card NFT badge.
+        
+        /// --INPUTS--
+        /// * `payment` | `Bucket` - Takes a `Bucket` payment of Gumball Club tokens.
+    
+        /// --RETURNS--
+        /// * `(Bucket, Bucket)` - Returns a tuple of `Bucket`:
+        /// 1. Any refunded/leftover payments to the user.
+        /// 2. The `Bucket` of member card NFT badge.
         pub fn buy_member_card(&mut self, mut payment: Bucket) -> (Bucket, Bucket) {
             
             assert_eq!(
