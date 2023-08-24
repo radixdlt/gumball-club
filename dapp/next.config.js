a5cefd73f2b2657514f1b092a6b6c8c54ff59a14
Alex Stelea <alex.s@rdx.works>
2023-08-24 11:21:41 +0200

    build: update next config

--- dapp/next.config.js
+++ dapp/next.config.js
@@ -3,6 +3,18 @@ const dns = require('dns')
 dns.setDefaultResultOrder('ipv4first')
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    formats: [
+      'image/avif',
+      'image/webp',
+      'image/png',
+      'image/jpg',
+      'image/jpeg',
+      'image/svg+xml',
+    ],
+  },
+}
 
 module.exports = nextConfig
