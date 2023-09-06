a6b007df56cb15c8d35e5b67863ec1e3fa2996c8
Alex Stelea <alex.s@rdx.works>
2023-09-06 21:49:44 +0200

    chore: update addresses

--- dapp/src/app/config.ts
+++ dapp/src/app/config.ts
@@ -27,20 +27,20 @@ export const config = {
     sugarOracleComponent:
       'component_tdx_22_1cqkfzuxf9vdf5uacjdtqvgtcp6pwsn3pphrgh2ndk7kdujsredc6xx',
     gumballClubPackage:
-      'package_tdx_22_1p5cnytswfqytmna32rnpy9hq7wkeky0007wmhkgvx89jlkfg6yzj9p',
+      'package_tdx_22_1p4yckqa9r96mtvnwng4zgh44qs2rl0anzs008q7g49sjellmss833k',
     gumballClubComponent:
-      'component_tdx_22_1cqmwvdhry0tsth325r47d58d0d4aap0m79wt6fl6flehn4yr05d5hv',
+      'component_tdx_22_1cpk4qsey3stg98w82tshegphqls82fasrlqw0kaxjgdcwyfykuydka',
     gumballMachineComponent:
-      'component_tdx_22_1cp6yzct4yxp9gpn7vvf5a99mkfwn7l74vthxdawcfd9mtnrmzd0pm6',
+      'component_tdx_22_1czzcngdndhrlsg8a7dahrjk3f6k87gykudgwpanxcsqxem75swf80g',
     candyMachineComponent:
-      'component_tdx_22_1crjcyncwpgq2x9kdcyxm7frnx5zj4gy0zm2ln0p4tj9c0ulhsltra0',
+      'component_tdx_22_1cpmjz2etrhhtsx9pep9t93nnqzvkgzwp7n8ut8ujkenamh0ln34xda',
     gumballClubTokensResource:
-      'resource_tdx_22_1t4egu7yxw0j7kanyzxjvgv0sw7jed0stpq8fr8lwvgcnp7zzjyynvk',
+      'resource_tdx_22_1thf2myxtgfpd0na3m9zqzjmx8p27aysrgsup7tuhav53q3heqhfgev',
     gumballClubMemberCardResource:
-      'resource_tdx_22_1t4egu7yxw0j7kanyzxjvgv0sw7jed0stpq8fr8lwvgcnp7zzjyynvk',
+      'resource_tdx_22_1ntsrh2vd37ha3u9valkaq246wcjupt4u25am99l56r5vplalmygudw',
     gumballResource:
-      'resource_tdx_22_1th3ekrvnvavs5m4d4gc73kk0xydq25p5w4c2zzhpcnjnhvywl03uza',
+      'resource_tdx_22_1t54x444prmagsafx6wtnlvfkadqfewnyy28nfaqusdc47l8uahrpjx',
     candyTokenResource:
-      'resource_tdx_22_1tkf4hwpykjzrpz587y4yj24r3vwtqpr2zr7y7xn5purn3z22rwp2v5',
+      'resource_tdx_22_1t40j50t5evj6nhknlwvyv9wn95884df7yf0hwzah0jxw6627882xwa',
   },
 }
