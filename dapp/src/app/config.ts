ec43e2efc2c9cc72a4741c1d2e49266c3c6e67e1
Alex Stelea <alex.s@rdx.works>
2023-10-12 15:41:28 +0200

    chore: redeploy stokenet

--- dapp/src/app/config.ts
+++ dapp/src/app/config.ts
@@ -56,25 +56,25 @@ const gumballNetworkConfig = {
       'account_tdx_2_129nx5lgkk3fz9gqf3clppeljkezeyyymqqejzp97tpk0r8els7hg3j',
     addresses: {
       sugarOraclePackageAddress:
-        'package_tdx_2_1p4jtcqcwxmvzdp83uvr57pekt6fam4rruwxj5fms5pvzzmr3ra65fc',
+        'package_tdx_2_1p4lftg7zjtmvyw5dwv3fg9cxyumlrya03p5uecqdge9thje4nm5qtk',
       sugarOracleComponent:
-        'component_tdx_2_1crr5k3lnjq5ndtmexknwgd58tn473r7enf3rst43puklvlm5uff6u7',
+        'component_tdx_2_1cptzmfxvdwdy90euq98rldtsghk7pj549kukzfgl6xmlthmaw8262m',
       gumballClubPackage:
-        'package_tdx_2_1phjykvhkpyap6fg5433a0akam0lc8vgzd4cnf5slhm0y6p0m0kw4c9',
+        'package_tdx_2_1pkaw4m82c89hy0gk4dwqtqlln6md8anr2ysnrvegxar53mr6nvn5ay',
       gumballClubComponent:
-        'component_tdx_2_1czgve6uw9tnvc4udc3nx6gnhxqjn488yhttpkr3kjhx7f7jzpd8c7t',
+        'component_tdx_2_1czg6rq9vms7t402fedtpzkjah25hh7snyu3ysgxk3pwlz4d3tugm7j',
       gumballMachineComponent:
-        'component_tdx_2_1cpagaqy73p9a7v4cx23ayn5cgfcqxet90sclvluk6mca2rzw38hfd5',
+        'component_tdx_2_1cq64nku32ypaytze8nys60y2zjvsmquxxe02v6ksh4exqkkqpxgrqq',
       candyMachineComponent:
-        'component_tdx_2_1cze3ujjsymjsetxrgaerhjp7x0zjc4c7wcduwk0g3ytxk9yln0cqvj',
+        'component_tdx_2_1crje3en7zsrna9t5vyywn3z3t9ht34l9udxjcpjvdhpcw9v6vlzru8',
       gumballClubTokensResource:
-        'resource_tdx_2_1t4pre2zseqavzsktmhtffkfc8kwqmydj6ejtft0chevyvwrjmmwftq',
+        'resource_tdx_2_1thqcgjw37fjgycpvqr52nx4jcsdeuq75mf2nywme07kzsuds9a4psp',
       gumballClubMemberCardResource:
-        'resource_tdx_2_1ntps5asnsjrajuef209kdjd779r84dnd473ltxtlga838rp6ez2zk6',
+        'resource_tdx_2_1ng88qk08hrgmad30rzdxpyx779yuta4cwcjc3gstk60jhachsv94g9',
       gumballResource:
-        'resource_tdx_2_1t4e3rx59pfumryng7r6ezm2tsu72lhtl4whj6wjryrquwnhm2yrtwf',
+        'resource_tdx_2_1t4kep9ldg9t0cszj78z6fcr2zvfxfq7muetq7pyvhdtctwxum90scq',
       candyTokenResource:
-        'resource_tdx_2_1thfegpdrkld3hdf5vlldwz8vhpvtmkkr78u6evguqwstvdv0k6zryd',
+        'resource_tdx_2_1tk30vj4ene95e3vhymtf2p35fzl29rv4us36capu2rz0vretw9gzr3',
     } satisfies Addresses,
   },
 }[network.networkName]
